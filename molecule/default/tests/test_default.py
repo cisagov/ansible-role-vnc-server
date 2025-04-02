6c52ceaf0b63f6978d6291b4823d214c1c369103
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2025-04-02 15:42:13 -0400

    Fix code that disables screen locking
    Kali used to lock the screen using light-locker but now has switched
    to using xfce4-power-manager and xfce4-screensaver.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -4,6 +4,7 @@
 import os
 
 # Third-Party Libraries
+import pytest
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
@@ -25,3 +26,16 @@ def test_packages(host):
     installed = [package.is_installed for package in packages]
     assert len(pkgs) != 0
     assert all(installed)
+
+
+@pytest.mark.parametrize(
+    "f",
+    [
+        "/home/vnc/.config/autostart/xfce4-power-manager-disable-dpms.desktop",
+        "/home/vnc/.config/autostart/xfce4-screensaver-disable.desktop",
+        "/home/vnc/.config/autostart/xfce4-screensaver-disable-screen-locking.desktop",
+    ],
+)
+def test_autostart_files_exist(host, f):
+    """Test that the file exists."""
+    assert host.file(f).exists
